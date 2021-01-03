b67d8e122f871f453baed9aea7509af57b17fb68
Hafizh H <hafizh.h3d@gmail.com>
2021-01-04 03:25:11 +0700

    Added Menu Function

--- Assignment 2 Project.cpp
+++ Assignment 2 Project.cpp
@@ -2,9 +2,28 @@
 
 using namespace std;
 
+void menu()
+{
+	cout << "=============================" << endl;
+	cout << "Hotel Management System" << endl;
+	cout << "=============================" << endl << endl;
+	
+	cout << "1. Check Available Room" << endl;
+	cout << "2. Book Room" << endl;
+	cout << "3. Exit" << endl << endl;
+	
+	cout << "Choose Option : ";
+}
+
 int main ()
 {
-	cout << "Hotel Mangement System" << endl;
+	int choice = 0; // Variable for Choice
+	
+	while (choice != 3)
+	{
+		menu();
+		cin >> choice;
+	}
 	
 	return 0;
 }
